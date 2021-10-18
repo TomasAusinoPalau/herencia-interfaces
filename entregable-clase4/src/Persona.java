042bb55138f00dd0c2aee9b6b8716d1f9e197f94
Tomas <tausinopalau@gmail.com>
2021-10-18 08:25:21 -0300

    getters and setters persona

--- entregable-clase4/src/Persona.java
+++ entregable-clase4/src/Persona.java
@@ -11,7 +11,31 @@ public class Persona implements ISerVivo  {
         this.genero = genero;
         this.nutricion = nutricion;
     }
-    
+
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public int getCantidadPatas() {
+        return cantidadPatas;
+    }
+    public void setCantidadPatas(int cantidadPatas) {
+        this.cantidadPatas = cantidadPatas;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getNutricion() {
+        return nutricion;
+    }
+    public void setNutricion(String nutricion) {
+        this.nutricion = nutricion;
+    }    
     public void caminar(){
         System.out.println("La persona esta caminando");
     }
